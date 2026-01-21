9021ed2e11a7ffebba456ef28b686f75e70f345e
Felix Graham <felix.graham25@students.bhasvic.ac.uk>
2026-01-21 15:43:45 +0000

    world collision, movement and block breaking

--- MyWorld.java
+++ MyWorld.java
@@ -9,42 +9,49 @@ public class MyWorld extends World
         super(600, 400, 1); // create world
 
         generate();
+        prepare();
     }
 
- 
     public void generate(){
         /*for (int i=0; i < 600; i=i+22){ // for all x
-            int layers = Greenfoot.getRandomNumber(5); // determines how many blocks y
-            int yPos = 389;
-            for(int j=0; j<layers; j++){
-            //for(int j=0; j<layers; j++){// for all y
-                Dirt dirt = new Dirt(); // init dirt
-                addObject(dirt, i, yPos);// new dirt
-                yPos = yPos-21;
-            }
-             
-            yPos = yPos-21;
-            
-            Grass grass = new Grass();
-            addObject(grass, i, yPos);     
-          */  
-            
-         
-         // for y+
+        int layers = Greenfoot.getRandomNumber(5); // determines how many blocks y
+        int yPos = 389;
+        for(int j=0; j<layers; j++){
+        //for(int j=0; j<layers; j++){// for all y
+        Dirt dirt = new Dirt(); // init dirt
+        addObject(dirt, i, yPos);// new dirt
+        yPos = yPos-21;
+        }
+
+        yPos = yPos-21;
+
+        Grass grass = new Grass();
+        addObject(grass, i, yPos);     
+         */  
+
+        // for y+
         int xpos = 0;
         int ypos = 389;
-	for(int v=389; v>329; v=v-20){
-		for(int w=0;w<601;w=w+22){ 
-		    Dirt dirt = new Dirt();
-		    addObject(dirt, w, v);
-		    xpos += 22;
-		    //ypos -= 20;
-		}
-	}
-         
-         
+        for(int v=389; v>229; v=v-20){
+            for(int w=0;w<601;w=w+22){ 
+                Dirt dirt = new Dirt();
+                addObject(dirt, w, v);
+                xpos += 22;
+                //ypos -= 20;
+            }
         }
+
     }
+    /**
+     * Prepare the world for the start of the program.
+     * That is: create the initial objects and add them to the world.
+     */
+    private void prepare()
+    {
+        player player = new player();
+        addObject(player,334,118);
+    }
+}
     
     /**
      * Prepare the world for the start of the program.
--- player.java
+++ player.java
@@ -4,27 +4,77 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 public class player extends Actor
 {
     
-    public void act()
-    {
-        // collision:
-	// each second pass dir as `d` for gravity.
-	// use a basic moveset to determine dir for `l` and `r`
-	// additionally use `space` to indicate `u`
-	
-	if(greenfoot.isKeyDown("right")){
-		MoveWithCollision("r");
-	}
-	
-	
-            
+    
+    public void gravity(){
+        // if underneath is nothing then fall by 1
+        if(getObjectsAtOffset(0, 20, null).size() < 1 || getY() > 100 ){
+            setLocation(getX(), (getY()+1));        
+        } else{
+        }
     }
     
+    public int collideDown(){
+        if(getObjectsAtOffset(0, 20, null).size() == 0){
+            return 1;
+        } else{
+            return -1;
+        }
+    }
+    
+    public void moveset(){
+        if(Greenfoot.isKeyDown("d")){
+            move(1);
+        } else if(Greenfoot.isKeyDown("a")){
+            move(-1);
+        } else if(Greenfoot.isKeyDown("space")){
+            setLocation(getX(), (getY()-2));
+        }
+    }
+    
+    public void mine(){
+        MouseInfo mouse = Greenfoot.getMouseInfo();
+        if(mouse != null && mouse.getButton() == 1){
+            MyWorld world = (MyWorld) getWorld();
+            if(Greenfoot.getMouseInfo().getActor() != this){
+                world.removeObject(Greenfoot.getMouseInfo().getActor());
+            }
+        }
+    }
+    
+    public void act(){
+        if(collideDown() == 1){
+            gravity();
+        }
+        moveset();
+        mine();
+    }}
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+ /*   
     
     public void MoveWithCollision(String dir){
         // dir as l/r/u/d
         // 1- get dir as offset val (x/y)
         // 2- offer reduced moveset
-	// 3- gravity. (lore accurate)
+    // 3- gravity. (lore accurate)
         
         //1:
         int offset = 0;
@@ -46,26 +96,26 @@ public class player extends Actor
             }
         
         //2:
-		 // if there is object to adjecent to attempted move:
-		 // then block move in that direction 
-	int obstacles = getObjectsAtOffset(0, offset, null).size();
+         // if there is object to adjecent to attempted move:
+         // then block move in that direction 
+    int obstacles = getObjectsAtOffset(0, offset, null).size();
+}}
+    // moveset:
+    
+        /*public void go(int rq, int obstacles){
+            // where rq is integer 0/1 for query is moving right
+            
+            if(obstacles = 0){
+                if(rq == 1){
+                    move(1);
+                } else{
+                    move(-1);
+                }
+            }
+            }
 
-	// moveset:
-	
-	public void go(int rq, int obstacles){
-		// where rq is integer 0/1 for query is moving right
-		
-		if(obstacles = 0){
-			if(rq == 1){
-				move(1);
-			} else{
-				move(-1);
-			}
-		}
-        }
-
-	go(1, obstaces);
+        go(1, obstaces);
 
 
-    }
-
+    }}
+*/
